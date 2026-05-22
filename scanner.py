03fb758a3b6d92101b0cbd4e58e24592d751361d
Nacoamante <dragonixperperoni@gmail.com>
2026-05-22 04:45:56 -0400

    feat: escaner de rango de puertos( 1-1024)

--- scanner.py
+++ scanner.py
@@ -3,19 +3,31 @@ import socket
 
 def scan_port(host: str, port: int) -> bool:
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.settimeout(1.0)
+
+    # Reducimos el tiempo de espera
+    sock.settimeout(0.1)
+
     result = sock.connect_ex((host, port))
     sock.close()
+
     return result == 0
+
+
+def puertosAbiertos(host):
+    contador = 0
+
+    for puerto in range(1, 1025):
+        if scan_port(host, puerto):
+            print(f"[+] Puerto {puerto} abierto")
+            contador += 1
+
+    print(f"\nPuertos abiertos encontrados: {contador}")
 
 
 def main():
     host = "127.0.0.1"
-    port = 80
-    if scan_port(host, port):
-        print(f"[+] Puerto {port} abierto en {host}")
-    else:
-        print(f"[-] Puerto {port} cerrado en {host}")
+
+    puertosAbiertos(host)
 
 
 if __name__ == "__main__":
